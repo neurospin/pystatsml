de0c666ccf8e0a9541cfe7d9ecb30a98b0f9d604
duchesnay <edouard.duchesnay@gmail.com>
2016-04-29 00:20:16 +0200

    Dummy coding

--- python/datasets.py
+++ python/datasets.py
@@ -47,8 +47,12 @@ Do it yourself
 --------------
 '''
 import numpy as np
+import scipy
 
-# Dataset
+#############
+# 2D: Dataset
+#############
+
 n_samples, n_features = 100, 2
 mean0, mean1 = np.array([0, 0]), np.array([0, 2])
 Cov = np.array([[1, .8],[.8, 1]])
@@ -61,6 +65,35 @@ y = np.array([0] * X0.shape[0] + [1] * X1.shape[0])
 n_samples, n_features,  = 100, 2
 
 np.random.randn()
+
+##############################################
+# Large Dataset with block diagonal covariance
+##############################################
+import numpy as np
+import scipy
+
+n_samples = 100
+block_size = 3
+n_block = 2
+n_features = block_size * n_block
+n_informatives = 2
+cov = .8
+var = 1
+
+# Block diagonal covariance
+Cov = scipy.linalg.block_diag(
+    *[np.zeros((block_size, block_size)) + cov for i in range(n_block)])
+np.fill_diagonal(Cov, var)
+
+    
+mean0, mean1 = np.zeros(n_features), np.zeros(n_features)
+mean1[:n_informatives] = 1
+
+np.random.seed(42)
+X0 = np.random.multivariate_normal(mean0, Cov, n_samples)
+X1 = np.random.multivariate_normal(mean1, Cov, n_samples)
+X = np.vstack([X0, X1])
+y = np.array([0] * X0.shape[0] + [1] * X1.shape[0])
 
 
 '''
