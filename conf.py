8b4d66b7f472e32642dee49fbd41c868c940bac7
Tommy Löfstedt <tommy.lofstedt@umu.se>
2016-09-23 20:13:04 +0200

    BUG: Fixed LaTeX error.

--- conf.py
+++ conf.py
@@ -214,7 +214,9 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    'preamble': '''
+        \\usepackage{amsfonts}
+    ''',
 
 # Latex figure (float) alignment
 #'figure_align': 'htbp',
