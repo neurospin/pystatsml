296dd7dc66f5df34ae098d84a1cd5930a8eb9a79
chl <chl@aliquote.org>
2019-04-23 12:36:23 +0200

    Linting + minor additions

--- R/stat_multiv_exo.R
+++ R/stat_multiv_exo.R
@@ -4,10 +4,10 @@ set.seed(42)
 
 ### Dot product and Euclidean norm
 
-a = c(2,1)
-b = c(1,1)
+a = c(2, 1)
+b = c(1, 1)
 
-euclidian <-function(x){
+euclidian <- function(x){
   return(sqrt(x %*% x)[1])
 }
 
@@ -17,29 +17,44 @@ euclidian(a - b)
 
 b %*% (a / euclidian(a))
 
-X = matrix(rnorm(100*2), 100, 2)
+X = matrix(rnorm(100 * 2), 100, 2)
 dim(X)
 X %*% (a / euclidian(a))
 
 ### Compute row means and store them into a vector
 
-row_means = function(X){
-  #means = matrix(NA, dim(X)[1], 1)
+row_means = function(X) {
   means = NULL
-  for(i in 1:dim(X)[1]){
-    #means[i, 1] = mean(X[i,])
-    means = c(means, mean(X[i,]))
+  for (i in 1:dim(X)[1]) {
+    means = c(means, mean(X[i, ]))
   }
   return(means)
 }
 row_means(X)
+
+## version 2, using built-in accessors
+
+row_means <- function(X) {
+  n <- nrow(X)
+  means <- numeric(n)
+  for (i in 1:n)
+    means[i] <- mean(X[i, ])
+
+  return(means)
+}
+
+## version 3, using apply
+apply(X, 1, mean)
+
+## version 4, more efficient
+rowMeans(X)
 
 ### Covariance matrix and Mahalanobis norm
 
 N = 100
 mu = c(1, 1)
 Cov = matrix(c(1, .8,
-              .8, 1), 2, 2)
+               .8, 1), 2, 2)
 
 library(MASS)
 X = mvrnorm(N, mu, Cov)
@@ -65,20 +80,17 @@ mahalanobis <- function(x, xbar, Sinv){
 }
 
 
-#dists = pd.DataFrame(
-
-dist = matrix(nrow=N, ncol=2)
+dist = matrix(nrow = N, ncol = 2)
 
 for(i in 1:nrow(X)){
     dist[i, 1] = mahalanobis(X[i, ], xbar, Sinv)
     dist[i, 2] = euclidian(X[i, ] - xbar)
 }
-colnames(dist) = c('Mahalanobis', 'Euclidian')
+colnames(dist) = c("Mahalanobis", "Euclidian")
 
 print(dist[1:10, ])
 
 x = X[1, ]
 
-library(stats)
-print(sqrt(stats::mahalanobis(X[1, ], xbar, Sinv, inverted=TRUE)) - mahalanobis(X[1, ], xbar, Sinv))
-
+print(sqrt(stats::mahalanobis(X[1, ], xbar, Sinv, inverted = TRUE)) -
+      mahalanobis(X[1, ], xbar, Sinv))
--- R/tools_R_exo.R
+++ R/tools_R_exo.R
@@ -1,52 +1,56 @@
-#df = read.csv(url('https://raw.github.com/neurospin/pystatsml/master/data/iris.csv'))
-setwd("/home/ed203246/git/pystatsml/R")
-df = read.csv('../data/iris.csv')
+# Set current working directory
+WD = "."
+setwd(WD)
 
-#Print column names
+df = read.csv("../datasets/iris.csv")
+
+# Print column names
 colnames(df)
 
-
-#Get numerical columns
+# Get numerical columns
 unlist(lapply(df, is.numeric))
 num_cols = colnames(df)[unlist(lapply(df, is.numeric))]
 
 # For each species compute the mean of numerical columns and store it in a stats table like:
 stats = NULL
-for(grp in levels(df$species)){
-  #grp = levels(df$species)[1]
-    m = as.matrix(df[df$species == grp, num_cols])
-  line = data.frame(species=grp, as.list(colMeans(m)))
+for (grp in levels(df$species)) {
+  m = as.matrix(df[df$species == grp, num_cols])
+  line = data.frame(species = grp, as.list(colMeans(m)))
   stats = rbind(stats, line)
 }
 
 print(stats)
 
+## shorter version
+aggregate(. ~ species, data = df, mean)
 
-user1 = data.frame(name=c('eric', 'sophie'),
-                   age=c(22, 48), gender=c('M', 'F'),
-                   job=c('engineer', 'scientist'))
-
-user2 = data.frame(name=c('alice', 'john', 'peter', 'julie', 'christine'),
-                   age=c(19, 26, 33, 44, 35), gender=c('F', 'M', 'M', 'F', 'F'),
-                   job=c("student", "student", 'engineer', 'scientist', 'scientist'))
+## Merging database
+user1 = data.frame(name = c("eric", "sophie"),
+                   age = c(22, 48),
+                   gender = c("M", "F"),
+                   job = c("engineer", "scientist"))
+user2 = data.frame(name = c("alice", "john", "peter", "julie", "christine"),
+                   age = c(19, 26, 33, 44, 35),
+                   gender = c("F", "M", "M", "F", "F"),
+                   job = c("student", "student", "engineer", "scientist", "scientist"))
 user3 = rbind(user1, user2)
+salary = data.frame(name = c("alice", "john", "peter", "julie"),
+                    salary = c(2200, 2400, 3500, 4300))
 
-salary = data.frame(name=c('alice', 'john', 'peter', 'julie'), salary=c(2200, 2400, 3500, 4300))
-user = merge(user3, salary, by="name", all=TRUE)
+user = merge(user3, salary, by = "name", all = TRUE)
 
 
 df = user
 
 fillmissing_with_mean <- function(df){
   num_cols = colnames(df)[unlist(lapply(df, is.numeric))]
-  for(n in num_cols){
-    # n = num_cols[1]
+  for (n in num_cols) {
     x = df[, n]
-    df[is.na(x), n] = mean(x[!is.na(x)]) # mean(x, na.rm=TRUE)
+    df[is.na(x), n] = mean(x[!is.na(x)])  # mean(x, na.rm=TRUE)
   }
   return(df)
 }
 
 user_imputed = fillmissing_with_mean(user)
 
-write.csv(user_imputed, "users_imputed.csv", row.names=FALSE)
+write.csv(user_imputed, "users_imputed.csv", row.names = FALSE)
